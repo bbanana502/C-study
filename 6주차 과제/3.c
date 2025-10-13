c107fcca5bfca21eedcaaa6b8f7c19a372883bfe
ㅠㅠ무뭄 <taehug662@gmail.com>
2025-10-13 11:42:55 +0900

    2완료

--- 6주차 과제/3.c
+++ 6주차 과제/3.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void front(int num);
+
+void center(int num);
+
 typedef struct Node {
     int data;
     struct Node* next;
@@ -39,6 +43,29 @@ int main(void)
 
         temp = newNode;
     }
+
+    int feat = 0;
+    int value = 0;
+    printf("어떤 기능을 수행하시겠습니까?(1: 제일 처음에 삽입  2: 정가운데에 삽입) : ");
+    scanf("%d",&feat);
+    printf("삽입하고 싶은 값을 입력해주세요 : ");
+    scanf("%d",&value);
+    if (feat == 1)
+    {
+        front(value);
+    }
+    else if (feat == 2)
+    {
+        center(value);
+    }
+}
+
+void front(int num){
+    
+}
+
+void center(int num){
+
 }
 
 // 노드의 개수를 입력받고 연결리스트 생성, 데이터 입력받아서 저장

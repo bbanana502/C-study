3b75156e01feda5e100feda61c756124bded4352
ㅠㅠ무뭄 <taehug662@gmail.com>
2025-10-13 16:49:40 +0900

    3완료

--- 6주차 과제/3.c
+++ 6주차 과제/3.c
@@ -1,24 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void front(int num);
-
-void center(int num);
-
 typedef struct Node {
     int data;
     struct Node* next;
 } Node;
 
+void front(int num);
+void center(int num, int n);
+
+Node* head = NULL;
+Node* temp = NULL;
+Node* newNode = NULL;
+int num;
+
 int main(void)
 {
-    Node* head = NULL;
-    Node* temp = NULL;
-    Node* newNode = NULL;
-    int num;
     printf("몇개의 노드를 입력하시겠습니까? : ");
     scanf("%d",&num);
-    int n;
     
     for (int i=0; i<num; i++)
     {
@@ -46,7 +45,7 @@ int main(void)
 
     int feat = 0;
     int value = 0;
-    printf("어떤 기능을 수행하시겠습니까?(1: 제일 처음에 삽입  2: 정가운데에 삽입) : ");
+    printf("어떤 기능을 수행하시겠습니까?  (1: 제일 처음에 삽입  2: 정가운데에 삽입) : ");
     scanf("%d",&feat);
     printf("삽입하고 싶은 값을 입력해주세요 : ");
     scanf("%d",&value);
@@ -56,16 +55,46 @@ int main(void)
     }
     else if (feat == 2)
     {
-        center(value);
+        center(value, num);
     }
+    printf("결과 출력 : ");
+    for (Node* i=head; i!=NULL; i=i->next)
+    {
+        printf("%d ",i->data);
+    }
+    return 0;
 }
 
 void front(int num){
-    
+    Node* NEW;
+    NEW = (Node*)malloc(sizeof(Node));
+    if (NEW == NULL)
+    {
+        printf("에러");
+        return;
+    }
+    NEW->data = num;
+    NEW->next = head;
+    head = NEW;
 }
 
-void center(int num){
-
+void center(int num, int n)
+{
+    Node* NEW;
+    NEW = (Node*)malloc(sizeof(Node));
+    if (NEW == NULL)
+    {
+        printf("에러");
+        return;
+    }
+    NEW->data = num;
+    Node* temp = head;
+    for (int i=0; i<(n/2)-1; i++)
+    {
+        temp = temp->next;
+    }
+    NEW->next = temp->next;
+    temp->next = NEW;
 }
 
 // 노드의 개수를 입력받고 연결리스트 생성, 데이터 입력받아서 저장
